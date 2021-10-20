37f999037aacf26b220679acaf1081e61d2ae8e7
Halamai <halamaipawlo@gmail.com>
2021-10-20 16:33:05 +0300

    dddddddddd

--- src/redux/contacts/contacts-selectors.js
+++ src/redux/contacts/contacts-selectors.js
@@ -2,7 +2,7 @@ import { createSelector } from "reselect";
 const contactsErrorSelector = (state) => state.contacts.error;
 const contactsIsLoadingSelector = (state) => state.contacts.isLoading;
 const contactsContactsgSelector = (state) => state.contacts.items;
-const contactsFilterSelector = (state) => state.filter;
+const contactsFilterSelector = (state) => state.contacts.filter;
 
 export const getWisibileContacts = createSelector(
   [contactsContactsgSelector, contactsFilterSelector],
